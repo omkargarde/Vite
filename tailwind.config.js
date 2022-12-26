29629ac97fc85769b1f396f9fd2641cd3bc315ce
omkar-garde <omyagarde@gmail.com>
2022-12-26 11:43:17 +0530

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  purge: ["./src/components/**/*.{js,jsx,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
