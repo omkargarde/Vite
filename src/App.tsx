91df72260b52ed0d29c6365714df8f04aaa72112
omkargarde <42486951+omkargarde@users.noreply.github.com>
2022-12-26 11:19:56 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import AppFooter from "./components/AppFooter";
+import AppHeader from "./components/AppHeader";
+
+function App() {
+  return (
+    <>
+      <AppHeader></AppHeader>
+      <h1>bodyqweqw</h1>
+      <AppFooter></AppFooter>
+    </>
+  );
+}
+
+export default App;
