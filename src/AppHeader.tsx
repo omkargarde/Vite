d028a85ba40b99a232d812a9814902ffee727a5c
omkargarde <42486951+omkargarde@users.noreply.github.com>
2022-12-26 11:21:29 +0530

    Create AppHeader.tsx

---
+++ src/AppHeader.tsx
@@ -0,0 +1,20 @@
+function AppHeader() {
+  return (
+    <>
+      <header>
+        <nav className="shadow-md px-6 py-3 bg-white flex   justify-between">
+          <button className="p-3 bg-cyan-200	rounded-lg hover:bg-white  hover:shadow-lg ">
+            button1
+          </button>
+          <button className="p-3 bg-cyan-200	rounded-lg hover:bg-white  hover:shadow-lg ">
+            button2
+          </button>
+          <button className="p-3 bg-cyan-200	rounded-lg hover:bg-white  hover:shadow-lg ">
+            button3
+          </button>
+        </nav>
+      </header>
+    </>
+  );
+}
+export default AppHeader;
