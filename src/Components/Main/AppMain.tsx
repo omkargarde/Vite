2369bdaa5c0c9003dda10a06baf772d753fcb927
omkar-garde <omyagarde@gmail.com>
2023-01-26 17:49:55 +0530

    listing

--- src/Components/Main/AppMain.tsx
+++ src/Components/Main/AppMain.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 import { getData } from "../endpoints/GetApi";
-import Card from "./Card";
+import Card from "./shared/Card";
 import Facet from "./Facet";
 import SearchBox from "./shared/SearchBox";
+import Listing from "./shared/Listing";
 export default function AppMain() {
   const { data, error, isError, isLoading, refetch } = useQuery(
     // first argument is a string to cache and track the query result
@@ -11,6 +12,7 @@ export default function AppMain() {
     // second argument is query fn,(axios,fetch etc)
     getData
   );
+
   const [searchTerm, setSearchTerm] = useState("");
   if (isLoading) {
     return <div>Loading...</div>;
@@ -21,20 +23,18 @@ export default function AppMain() {
   }
   return (
     <div className="flex">
-      <div className="w-2/12">
+      {/* <div className="w-2/12">
         <Facet></Facet>
-      </div>
-      <div>
-        {/* lifting stateUp */}
-        <SearchBox
-          searchTerm={searchTerm}
-          setSearchTerm={setSearchTerm}
-        ></SearchBox>
-        <div className="mx-auto grid grid-cols-1 gap-6 px-3 sm:grid-cols-2 lg:grid-cols-5">
-          {data.map((post: any, index: any) => {
-            return <Card key={index} card={post}></Card>;
-          })}
+      </div> */}
+      <div className="w-full">
+        <div className="mx-auto w-11/12 md:w-1/2">
+          {/* lifting stateUp */}
+          <SearchBox
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+          ></SearchBox>
         </div>
+        <Listing searchTerm={searchTerm} data={data}></Listing>
       </div>
     </div>
   );
