b42ad55f9bca2784f932a1f9976803d1679e6ac8
omkar-garde <omyagarde@gmail.com>
2023-02-21 15:27:43 +0530

    code clean up

--- client/src/Components/Shared/SearchBox.tsx
+++
@@ -1,5 +0,0 @@
-// TODO add this to obsidian https://beta.reactjs.org/learn/sharing-state-between-components#lifting-state-up-by-example
-export default function SearchBox(){
-    return <>
-    input</>
-}
